62434475a278f701fa7e8fac7c615d4224c4b90e
franksosa14 <gonza123e@gmail.com>
2021-11-10 15:20:42 -0300

    filtros de orden terminados

--- javascript/main.js
+++ javascript/main.js
@@ -1,28 +1,30 @@
 const endpoint = `https://apipetshop.herokuapp.com/api/articulos`
+let chamber = document.title === "Tienda-Medicamento" ? "Medicamento" : "Juguete"
 
-fetch(endpoint,)
-.then(api => api.json())
-.then(datas => {
-    myProgram(datas)
-})
+fetch(endpoint, )
+    .then(api => api.json())
+    .then(datas => {
+        myProgram(datas)
+    })
 
 
-function myProgram(data){
-    
+function myProgram(data) {
+
     let articulos = data.response
     console.table(articulos)
 
-function filtroStock(array) {
-    let arrayGeneral = [...array];
-    
-    let arrayfilter = arrayGeneral.filter((element) => element.stock <= 5);
-    
-    return arrayfilter;
-}
+    function filtroStock(array) {
+        let arrayGeneral = [...array];
+
+        let arrayfilter = arrayGeneral.filter((element) => element.stock <= 5);
+
+        return arrayfilter;
+    }
     let menos5 = filtroStock(articulos)
 
     let carousel = document.querySelector("#carousells")
-    function imprimirCarousel (array){
+
+    function imprimirCarousel(array) {
         array.forEach(element => {
             carousel.innerHTML += `
             <div class="carousel__face" style= "background-image: url(${element.imagen});">
@@ -32,38 +34,79 @@ function filtroStock(array) {
         })
     }
     imprimirCarousel(menos5)
-    
-    // ARMAR FUNCION MOSTRAR ARTICULOS POR MEDICAMENTO Y JUGETE :: ESTEBAN Y MIGUEL
-    
-    if(document.title == "Franco || Jugetes"){
-        inyectarDiv(articulos, "articulosJuguetes", "Juguete")
-    }
-    else {
-        inyectarDiv(articulos, "medicamentos", "Medicamento")
-    }
 
-    function inyectarDiv (articulos, id, tipo) {
-            let bodyStore = document.querySelector(`#${id}`)
-            articulos.forEach(articulo => {
-                if(articulo.tipo === `${tipo}`){        
-                    bodyStore.innerHTML += `
-                    <div class="col">
-                        <div class="card">
-                            <img src= ${articulo.imagen}>
-                            <div class="card-body">
-                                <p>Nombre: ${articulo.nombre}</p>
-                                <p>Stock: ${articulo.stock <= 5 ? "Ultimas unidades!" : articulo.stock}</p>
-                                <div class="precio-stock">
-                                    <p>$${articulo.precio}</p>
-                                    <button>Agregar al Carrito</button>
-                                </div>
-                            </div>
+    // ARMAR FUNCION MOSTRAR ARTICULOS POR MEDICAMENTO Y JUGETE :: ESTEBAN Y MIGUEL
+
+    inyectarDiv(articulos)
+
+    function inyectarDiv(articulos) {
+        const catalogo = document.querySelector("#articulos");
+
+        catalogo.innerHTML = ""
+        articulos.forEach(articulo => {
+            if (articulo.tipo === `${chamber}`) {
+                catalogo.innerHTML += `
+                <div class="col">
+                <div class="card">
+                    <img src= ${articulo.imagen}>
+                    <div class="card-body">
+                        <p>Nombre: ${articulo.nombre}</p>
+                        <p>Stock: ${articulo.stock <= 5 ? "Ultimas unidades!" : articulo.stock}</p>
+                        <div class="precio-stock">
+                            <p>$${articulo.precio}</p>
+                            <button>Agregar al Carrito</button>
                         </div>
                     </div>
-                    `
-                }
-            })
+                </div>
+            </div>
+        `;
+            }
+        })
     }
+    let mayor = document.getElementById("mayor");
+    let menor = document.getElementById("menor");
+
+
+
+    let articulosCopia = [...articulos]
+
+    mayor.addEventListener("change", () => {
+        if (mayor.checked === true) {
+            const artMayor = articulosCopia.sort((a, b) => {
+                if (b.precio < a.precio) {
+                    return -1;
+                }
+                if (b.precio > a.precio) {
+                    return 1;
+                }
+                return 0;
+
+            });
+            inyectarDiv(artMayor)
+        } else {
+            inyectarDiv(articulos);
+        }
+        //console.log(miembros);
+    })
+    menor.addEventListener("change", () => {
+        if (menor.checked === true) {
+            const artMayor = articulosCopia.sort((a, b) => {
+                if (a.precio < b.precio) {
+                    return -1;
+                }
+                if (a.precio > b.precio) {
+                    return 1;
+                }
+                return 0;
+
+            });
+            inyectarDiv(artMayor)
+        } else {
+            inyectarDiv(articulos);
+        }
+        //console.log(miembros);
+    })
+
 
 
 
