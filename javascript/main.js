89b58d9e5b3fa773fd11ae88edd8c298d7c6af2c
LautaroStein <steinlautaroweb@gmail.com>
2021-11-10 00:26:26 -0300

    generando rama developer

--- javascript/main.js
+++ javascript/main.js
@@ -8,20 +8,31 @@ fetch(endpoint,)
 
 
 function myProgram(data){
-let articulos = data
+
+
+let articulos = data.response
 console.table(articulos)
 // ARMAR FUNCION MOSTRAR ARTICULOS POR MEDICAMENTO Y JUGETE :: ESTEBAN Y MIGUEL
+function filtroStock(array) {
+    let arrayGeneral = [...array];
 
+    let arrayfilter = arrayGeneral.filter((element) => element.stock <= 5);
 
+    return arrayfilter;
+    }
+    let menos5 = filtroStock(articulos)
 
-
-
-
-
-
-
-
-
+    let carousel = document.querySelector("#carousells")
+    function imprimirCarousel (array){
+        array.forEach(element => {
+            carousel.innerHTML += `
+            <div class="carousel__face" style= "background-image: url(${element.imagen});">
+                <span>${element.nombre} </span>
+            </div>
+            `
+        })
+    }
+imprimirCarousel(menos5)
 
 
 
